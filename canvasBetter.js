a914abe98de42b7e14c490fbeba0b0e25b06eb11
Srulis <dgilogley@gmail.com>
2017-05-02 15:18:19 +1000

    Update to console log

--- canvasBetter.js
+++ canvasBetter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canvas betterments!
 // @namespace    https://siteadmin.instructure.com/
-// @version      2017.05.03.v3
+// @version      2017.05.03.03
 // @description  try to take over the world!
 // @author       Daniel Gilogley
 // @match        https://*.test.instructure.com/*
